4d917782e6931c7c21a8da0a45684540dde1faf0
AnselmoMauricioJunior <anselmo.mauricio.jr@gmail.com>
2023-08-04 11:55:50 -0300

    feat: Bat instalação de pacotes

---
+++ InstallPackage.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Iniciando instalacao telebot...
+py -m pip install telebot
+echo.
+echo Iniciando instalacao bardapi...
+py -m pip install bardapi
+echo.
+echo Iniciando instalacao load_dotenv...
+py -m pip install load_dotenv
+echo.
+echo Iniciando instalacao scrapy...
+py -m pip install scrapy
+echo.
+echo Iniciando instalacao xhtml2pdf...
+py -m pip install xhtml2pdf
+echo.
+echo Iniciando instalacao bs4...
+py -m pip install bs4
+echo.
+pause
